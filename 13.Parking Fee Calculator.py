8963c93db29911c775e27668952546c04a5616be
Jagan R <154572332+Jaganragupathi@users.noreply.github.com>
2026-07-27 13:54:14 +0530

    Delete 13.Parking Fee Calculator.py

--- 13.Parking Fee Calculator.py
+++
@@ -1,9 +0,0 @@
-vehicle = input("Enter Vehicle (Bike/Car): ")
-hours = int(input("Enter Hours: "))
-
-if vehicle == "Bike":
-    fee = hours * 20
-else:
-    fee = hours * 50
-
-print("Parking Fee =", fee)
