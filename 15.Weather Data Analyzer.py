128a47125fb080d2c4025e9beff8515288853b79
Jagan R <154572332+Jaganragupathi@users.noreply.github.com>
2026-07-27 13:56:34 +0530

    Delete 15.Weather Data Analyzer.py

--- 15.Weather Data Analyzer.py
+++
@@ -1,9 +0,0 @@
-temp = []
-
-for i in range(7):
-    t = int(input("Enter Temperature: "))
-    temp.append(t)
-
-print("Maximum =", max(temp))
-print("Minimum =", min(temp))
-print("Average =", sum(temp) / 7)
