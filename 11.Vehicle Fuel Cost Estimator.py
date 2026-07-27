9b793501003893a661916c82d80a1c013f9ba8c4
Jagan R <154572332+Jaganragupathi@users.noreply.github.com>
2026-07-27 13:53:42 +0530

    Delete 11.Vehicle Fuel Cost Estimator.py

--- 11.Vehicle Fuel Cost Estimator.py
+++
@@ -1,8 +0,0 @@
-distance = float(input("Enter Distance: "))
-mileage = float(input("Enter Mileage: "))
-price = float(input("Enter Fuel Price: "))
-
-fuel = distance / mileage
-cost = fuel * price
-
-print("Fuel Cost =", cost)
